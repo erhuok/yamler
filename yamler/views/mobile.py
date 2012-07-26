60210f442d0bc088297cb473616ed5980b1a39e6
yanghuzhi <huzhi.yang@foxmail.com>
2012-07-26 17:09:13 +0800

    register

--- yamler/views/mobile.py
+++ yamler/views/mobile.py
@@ -60,7 +60,7 @@ def register():
             #    row = g.db.execute(select([companies.c.id], and_(companies.c.name==request.form['company_name']))).fetchone()
             #    company_id = g.db.execute(companies.insert(), name=request.form['company_name'], user_id=user.id).inserted_primary_key[0] if row is None else row['id']
             g.db.execute(users.update().values({users.c.company_id: user.id, users.c.is_active: 1}).where(users.c.id==user.id))
-            url = 'http://'+request.host + '/i/' + base64.encodestring(str(g.company.id)) 
+            url = 'http://'+request.host + '/i/' + base64.encodestring(str(user.id)) 
             return jsonify(error=0, code='success', message='成功注册', user_id = user.id, url=url, realname=user.realname)
 
     return jsonify(error=1, code = 'no_username_or_password', message='没有输入用户名或密码')
